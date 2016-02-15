9309d9f1b7118476ce9b51fc6001b978db62babe
Peramanathan <prem@weavler.it>
2016-02-15 21:27:23 +0100

    Flask app removed and django app build from scratch

--- weavler/weavler/wsgi.py
+++ weavler/weavler/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wsadmin project.
+WSGI config for weavler project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsadmin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weavler.settings")
 
 application = get_wsgi_application()
