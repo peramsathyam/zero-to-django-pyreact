e56a8c75481ef6f5b67aec72a109217d3648feb7
Peramanathan <prem@weavler.it>
2016-02-16 16:42:06 +0100

    User  django shortcut to display the page

--- weavler/webshop/views.py
+++ weavler/webshop/views.py
@@ -8,9 +8,8 @@ from . models import Product
 def index(request):
     # Fetch first 3 products in reverese alphabetical orderin
     product_list = Product.objects.order_by('-title')[:3]
-    template = loader.get_template('webshop/index.html')
     context = {'product_list': product_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'webshop/index.html', context)
 
 
 def detail(request, product_id):
